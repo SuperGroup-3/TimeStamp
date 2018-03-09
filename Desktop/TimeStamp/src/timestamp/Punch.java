14492549b22aa1b1146b9a172f80dbab845ab1ad
Cody Johnson <cjohnson32@stu.jsu.edu>
2018-03-09 12:00:38 -0600

    Added new instance field to Badge Class

--- Desktop/TimeStamp/src/timestamp/Punch.java
+++ Desktop/TimeStamp/src/timestamp/Punch.java
@@ -9,16 +9,18 @@ import java.util.GregorianCalendar;
 public class Punch {
     
     //instance fields
+    private Badge badge;
     private int id;
     private int terminalid;
     private String badgeid;
     private GregorianCalendar originaltimestamp;
-    private int eventtypeid;
+    private int punchtypeid;
     private String eventdata;
     
     
     //constructor
-    public Punch(int id, int terminalid, String bagdeid, GregorianCalender originaltimestanp, int eventtypeid, String eventdata) {
+    public Punch(Badge badge, int id, int terminalid, String bagdeid, GregorianCalender originaltimestanp, int eventtypeid, String eventdata) {
+        this.badge = badge;
         this.id = id;
         this.terminalid = terminalid;
         this.badgeid = badgeid;
