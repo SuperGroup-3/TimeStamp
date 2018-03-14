721488a508449f7a6f05cc4da298d105ef84b422
Cody Johnson <cjohnson32@stu.jsu.edu>
2018-03-14 11:50:21 -0500

    Adjusted Main

--- Desktop/TimeStamp/src/timestamp/TimeStamp.java
+++ Desktop/TimeStamp/src/timestamp/TimeStamp.java
@@ -5,8 +5,8 @@ public class TimeStamp {
     public static void main(String[] args) {
         try {
             TASDatabase db= new TASDatabase();
-            Badge b1 = db.getBadge("12565C60");
-            Punch p = new Punch(b1, 1, 1);
+            Badge b2 = db.getBadge("08D01475");
+            Punch p = new Punch(b2, 1, 1);
             System.out.println(p.printOriginalTimestamp());
             
         }
