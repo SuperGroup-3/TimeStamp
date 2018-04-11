538446d172caa3caa95299209309f72d91f8fadc
Cody Johnson <cjohnson32@stu.jsu.edu>
2018-04-11 11:27:18 -0500

    Added Feature2.java to project

---
+++ Desktop/TimeStamp/test/Feature2.java
@@ -0,0 +1,48 @@
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import org.junit.*;
+import static org.junit.Assert.*;
+import timestamp.Punch;
+import timestamp.TASDatabase;
+
+public class Feature2 {
+    
+    private TASDatabase db;
+    
+    @Before
+    public void setup() throws ClassNotFoundException, InstantiationException, SQLException, IllegalAccessException {
+        db = new TASDatabase();
+    }
+    
+    @Test
+    public void testInsertCheckPunch() throws SQLException {
+		
+        /* Create New Punch Object */
+
+        Punch p1 = new Punch("021890C0", 101, 1);
+		
+        /* Get Punch Properties */
+        
+        String badgeid = p1.getBadgeid();
+        String originaltimestamp = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(p1.getOriginaltimestamp().getTime());
+        int terminalid = p1.getTerminalid();
+        int eventtypeid = p1.getPunchtypeid();
+		
+        /* Insert Punch Into Database */
+        
+        int punchid = db.insertPunch(p1);
+		
+        /* Retrieve New Punch */
+        
+        Punch p2 = db.getPunch(punchid);
+		
+        /* Compare Punches */
+
+        assertEquals(badgeid, p2.getBadgeid());
+        assertEquals(originaltimestamp, (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(p2.getOriginaltimestamp().getTime()));
+        assertEquals(terminalid, p2.getTerminalid());
+        assertEquals(eventtypeid, p2.getPunchtypeid());
+        
+    }
+
+}
