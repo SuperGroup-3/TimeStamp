707939a43d4c082da257921578a384f55e72cae8
Michael Harvey <mharvey4@stu.jsu.edu>
2018-04-18 11:14:40 -0500

    added feature 4 pseudo code

--- Desktop/TimeStamp/src/timestamp/TASLogic.java
+++ Desktop/TimeStamp/src/timestamp/TASLogic.java
@@ -9,6 +9,16 @@ import java.util.ArrayList;
 public class TASLogic {
 
     public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift){
-        //Stuff go here
+        int totalMinutes = 0;
+        boolean insidePair = false; 
+        for (int i=0;i<dailypunchlist.size();++i){
+        //check for clock in punch
+        //find matching clock out punch
+        //change timestamps to millis
+        //subtract clock out milli from clock in milli
+        //divide ^ by 1000 to convert to seconds
+        //divide ^ by 60 to get minutes
+        }
+        return totalMinutes;
     }
 }
