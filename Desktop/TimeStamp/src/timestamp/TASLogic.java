b12352a8c44b7363c45c56ce7fcabec71722b3e2
Michael Harvey <mharvey4@stu.jsu.edu>
2018-04-18 11:49:19 -0500

    continued work on feature 4

--- Desktop/TimeStamp/src/timestamp/TASLogic.java
+++ Desktop/TimeStamp/src/timestamp/TASLogic.java
@@ -12,12 +12,16 @@ public class TASLogic {
         int totalMinutes = 0;
         boolean insidePair = false; 
         for (int i=0;i<dailypunchlist.size();++i){
-        //check for clock in punch
-        //find matching clock out punch
-        //change timestamps to millis
-        //subtract clock out milli from clock in milli
-        //divide ^ by 1000 to convert to seconds
-        //divide ^ by 60 to get minutes
+            //check for clock in punch
+            if (dailypunchlist.get(i).getPunchtypeid()==1){
+                long in = dailypunchlist.get(i).getOriginaltimestamp().getTimeInMillis();
+                
+            }
+            //find matching clock out punch
+            //change timestamps to millis
+            //subtract clock out milli from clock in milli
+            //divide ^ by 1000 to convert to seconds
+            //divide ^ by 60 to get minutes
         }
         return totalMinutes;
     }
